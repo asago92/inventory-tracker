d44b1942c75f2eb3ca999b5f757fbfea2e4b3b62
asago92 <101930040+asago92@users.noreply.github.com>
2025-07-20 11:07:38 +0100

    Update create_account.py

--- create_account.py
+++ create_account.py
@@ -3,7 +3,4 @@ import streamlit as st
 def create_account():
     st.title("Create your account")
 
-pg = st.navigation([
-    st.Page("create_account.py", title="First page", icon="🔥"),
-])
-pg.run()
+
