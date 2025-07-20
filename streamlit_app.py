e7fc6e00d096e5055b008286bce651cd3b295622
asago92 <101930040+asago92@users.noreply.github.com>
2025-07-20 10:59:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,11 +11,7 @@ st.set_page_config(
     page_title="Inventory tracker",
     page_icon=":shopping_bags:",  # This is an emoji shortcode. Could be a URL too.
 )
-st.header("Inventory Tracker")
-"""
-    **Welcome to your store's inventory tracker!**
-    This page reads and writes directly from/to your inventory database.
-"""
+
 pages = {
     "Your account": [
         #st.Page("create_account.py", title="Create your account"),
@@ -29,6 +25,12 @@ pages = {
 
 pg = st.navigation(pages, position="top")
 pg.run()
+
+st.header("Inventory Tracker")
+"""
+    **Welcome to your store's inventory tracker!**
+    This page reads and writes directly from/to your inventory database.
+"""
 
 tab1, tab2, tab3 = st.tabs(["Inventory Tracker", "Units Left", "Best Selling Items"])
 # -----------------------------------------------------------------------------
