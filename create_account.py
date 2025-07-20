2795eb3422cda180a6563deb9ed5b86364bf5098
asago92 <101930040+asago92@users.noreply.github.com>
2025-07-20 11:06:11 +0100

    Update create_account.py

--- create_account.py
+++ create_account.py
@@ -1,10 +1,9 @@
 import streamlit as st
 
-def page2():
-    st.title("Second page")
+def create_account():
+    st.title("Create your account")
 
 pg = st.navigation([
-    st.Page("page1.py", title="First page", icon="🔥"),
-    st.Page(page2, title="Second page", icon=":material/favorite:"),
+    st.Page("create_account.py", title="First page", icon="🔥"),
 ])
 pg.run()
