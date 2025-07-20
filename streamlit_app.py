8fe3ffb7a5ad5c45189dfc338c152491a3110cac
asago92 <101930040+asago92@users.noreply.github.com>
2025-07-20 10:58:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,12 +18,12 @@ st.header("Inventory Tracker")
 """
 pages = {
     "Your account": [
-        st.Page("create_account.py", title="Create your account"),
-        st.Page("manage_account.py", title="Manage your account"),
+        #st.Page("create_account.py", title="Create your account"),
+        #st.Page("manage_account.py", title="Manage your account"),
     ],
     "Resources": [
-        st.Page("learn.py", title="Learn about us"),
-        st.Page("trial.py", title="Try it out"),
+        #st.Page("learn.py", title="Learn about us"),
+        #st.Page("trial.py", title="Try it out"),
     ],
 }
 
