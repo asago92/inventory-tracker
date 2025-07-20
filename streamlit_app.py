46e24b01c7c96a9fd09a4f42f51a2ba9f0188ddc
asago92 <101930040+asago92@users.noreply.github.com>
2025-07-20 11:13:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.set_page_config(
 
 pages = {
     "Your account": [
-        st.Page("create_account.py", title="Create your account", page_icon=":material/account_circle:"),
+        st.Page("create_account.py", title="Create your account", icon=":material/account_circle:"),
     ],
     "Contact": [
         st.Page("contact.py", title="Contact us"),
